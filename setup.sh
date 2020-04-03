9fbc6580503128dd7739261aadfd8e08c67d8162
Jacob Casper <jacob.thomas.casper@gmail.com>
2020-04-03 00:46:02 -0500

    Create database during setup

--- setup.sh
+++ setup.sh
@@ -5,3 +5,5 @@ groupadd mercuryms
 useradd -g mercuryms -d /usr/share/mercuryms -s $(which nologin) mercuryms
 chown mercuryms:mercuryms /usr/share/mercuryms
 chmod 700 /usr/share/mercuryms
+sqlite3 /usr/share/mercuryms/mercuryms.sqlite "$(cat migrations/*)"
+chown mercuryms:mercuryms /usr/share/mercuryms/mercuryms.sqlite
