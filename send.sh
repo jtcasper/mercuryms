5acbaa33340dd77aac29c45a5b4a0ff3be544c0d
Jacob Casper <jacob.thomas.casper@gmail.com>
2020-04-05 04:21:27 -0500

    Enable foreign keys on sent media insert

--- send.sh
+++ send.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sqlite3 mercuryms.sqlite "$(awk '/received/ {print "INSERT INTO SENT_MEDIA (MEDIA_ID) VALUES("$1");";}' <(sqlite3 mercuryms.sqlite "SELECT ID, PHONE_NUMBER, URI FROM MEDIA WHERE ID NOT IN (SELECT MEDIA_ID FROM SENT_MEDIA)" | timeout $TIMEOUT_SECONDS netcat $HOST $PORT))"
+sqlite3 mercuryms.sqlite "$(awk 'BEGIN {print "PRAGMA FOREIGN_KEYS = ON;"} /received/ {print "INSERT INTO SENT_MEDIA (MEDIA_ID) VALUES("$1");";}' <(sqlite3 mercuryms.sqlite "SELECT ID, PHONE_NUMBER, URI FROM MEDIA WHERE ID NOT IN (SELECT MEDIA_ID FROM SENT_MEDIA)" | timeout $TIMEOUT_SECONDS netcat $HOST $PORT))"
